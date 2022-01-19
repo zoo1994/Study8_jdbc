0fcd542892033bdc17ca8553384e93dce924943d
zoo1994 <composure1994@gmail.com>
2022-01-19 12:36:37 +0900

    .

---
+++ src/com/sj/s1/start/StartMain.java
@@ -0,0 +1,12 @@
+package com.sj.s1.start;
+
+public class StartMain {
+
+	public static void main(String[] args) {
+		System.out.println("DB 연동 테스트 시작");
+		
+		
+		System.out.println("DB 연동 테스트 끝");
+	}
+
+}
